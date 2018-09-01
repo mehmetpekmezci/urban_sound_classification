0dafa6a2388bac698522aabae718a419c25db314
mehmetpekmezci <mehmet.pekmezci.32@gmail.com>
2018-09-01 15:49:11 +0300

    MP 3.5.0 cnn resnet param tune

--- src/usc.3.5.0/header.py
+++ src/usc.3.5.0/header.py
@@ -107,7 +107,7 @@ INPUT_SIZE=TRACK_LENGTH
 DROP_OUT=0.5
 KEEP_PROB=DROP_OUT
 #FULLY_CONNECTED_LAYERS=[256,256,256,256,512]
-FULLY_CONNECTED_LAYERS=[512]
+FULLY_CONNECTED_LAYERS=[256]
 
 
 ##
@@ -122,13 +122,13 @@ FULLY_CONNECTED_LAYERS=[512]
 #CNN_POOL_X_SIZES        = np.array([ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
 #CNN_POOL_Y_SIZES        = np.array([ 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
 
-CNN_KERNEL_COUNTS       = np.array([128,32,32,32,32,32,32,32,32,32,32])
-CNN_KERNEL_X_SIZES      = np.array([  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1])
-CNN_KERNEL_Y_SIZES      = np.array([  3,  3,  3,  3,  2,  2,  2,  2,  2,  2,  2])
-CNN_STRIDE_X_SIZES      = np.array([  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1])
-CNN_STRIDE_Y_SIZES      = np.array([  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1])
-CNN_POOL_X_SIZES        = np.array([  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1])
-CNN_POOL_Y_SIZES        = np.array([  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2])
+CNN_KERNEL_COUNTS       = np.array([128,128,32,32,32,32,32,32,32,32,32,32])
+CNN_KERNEL_X_SIZES      = np.array([  1,  1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
+CNN_KERNEL_Y_SIZES      = np.array([  3,  3, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1])
+CNN_STRIDE_X_SIZES      = np.array([  1,  1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
+CNN_STRIDE_Y_SIZES      = np.array([  1,  1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
+CNN_POOL_X_SIZES        = np.array([  1,  1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
+CNN_POOL_Y_SIZES        = np.array([  2,  2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1])
  
 ##
 ## TRAINING PARAMETERS
