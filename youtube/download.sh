f6c2022ae72b10de0fe838db9d4ca7322831e9f5
mehmetpekmezci <mpekmezci@havelsan.com.tr>
2019-08-06 12:03:44 +0300

    MP

---
+++ youtube/download.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+mkdir -p downloads
+
+
+Running=true
+
+while Running
+do
+  Running=false
+  for i in $(cat list.txt)
+  do
+	name=$(echo $i | sed -e 's/##.*//')
+	url=$(echo $i | sed -e 's/.*##//')
+	if [ ! -f downloads/$name.m4a ]
+	then
+	   Running=true
+	   echo "downloading $name"
+           youtube-dl --extract-audio --audio-format m4a --output  downloads/$name.m4a $url
+	fi
+  done
+
+done
