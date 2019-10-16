dc007e0898c6e14974b12c135da5ef0f62626dcc
Mehmet Pekmezci <mehmet.pekmezci.32@gmail.com>
2019-10-16 22:28:58 +0300

    MP

---
+++ src/9.0.1-TF-TranferLearning-Sound2Vec-AutoEncoder-Dense/USCHeader.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+##
+## IMPORTS
+##
+import importlib
+math        = importlib.import_module("math")
+logging     = importlib.import_module("logging")
+tf          = importlib.import_module("tensorflow")
+urllib3     = importlib.import_module("urllib3")
+tarfile     = importlib.import_module("tarfile")
+csv         = importlib.import_module("csv")
+glob        = importlib.import_module("glob")
+sys         = importlib.import_module("sys")
+os          = importlib.import_module("os")
+argparse    = importlib.import_module("argparse")
+np          = importlib.import_module("numpy")
+librosa     = importlib.import_module("librosa")
+pandas      = importlib.import_module("pandas")
+time        = importlib.import_module("time")
+random      = importlib.import_module("random")
+datetime    = importlib.import_module("datetime")
+
