b43e471bb75df0ba1014f0be440a48483248c556
mehmetpekmezci <mpekmezci@havelsan.com.tr>
2020-02-23 01:14:53 +0300

    MP

---
+++ src/4.0.1.16-SplitInput-FourierCNN-CNN7-FC2-CNNMetric/header.py
@@ -0,0 +1,197 @@
+#!/usr/bin/env python3
+##
+## IMPORTS
+##
+import importlib
+math        = importlib.import_module("math")
+logging     = importlib.import_module("logging")
+tf          = importlib.import_module("tensorflow")
+urllib3     = importlib.import_module("urllib3")
+tarfile     = importlib.import_module("tarfile")
+csv         = importlib.import_module("csv")
+glob        = importlib.import_module("glob")
+sys         = importlib.import_module("sys")
+os          = importlib.import_module("os")
+argparse    = importlib.import_module("argparse")
+np          = importlib.import_module("numpy")
+librosa     = importlib.import_module("librosa")
+pandas      = importlib.import_module("pandas")
+time        = importlib.import_module("time")
+random      = importlib.import_module("random")
+datetime    = importlib.import_module("datetime")
+
+
+##
+## DEFAULT CONFIGS
+##
+
+##
+## DATA DIRECTORY NAMES
+##
+FOLD_DIRS = ['fold1','fold2','fold3','fold4','fold5','fold6','fold7','fold8','fold9','fold10']
+#FOLD_DIRS = ['fold1','fold10']
+FOLD_DIRS = []
+SCRIPT_DIR=os.path.dirname(os.path.realpath(__file__))
+SCRIPT_NAME=os.path.basename(SCRIPT_DIR)
+MAIN_DATA_DIR = SCRIPT_DIR+'/../../data/'
+RAW_DATA_DIR = MAIN_DATA_DIR+'/0.raw/UrbanSound8K/audio'
+CSV_DATA_DIR=MAIN_DATA_DIR+"/1.csv"
+NP_DATA_DIR=MAIN_DATA_DIR+"/2.np"
+LOG_DIR_FOR_LOGGER=SCRIPT_DIR+"/../../logs/logger/"+SCRIPT_NAME
+LOG_DIR_FOR_TF_SUMMARY=SCRIPT_DIR+"/../../logs/tf-summary/"+SCRIPT_NAME+"/"+str(datetime.datetime.fromtimestamp(time.time()).strftime('%Y.%m.%d_%H.%M.%S'))
+SAVE_DIR=SCRIPT_DIR+"/../../save/"+SCRIPT_NAME
+
+
+if not os.path.exists(LOG_DIR_FOR_TF_SUMMARY):
+    os.makedirs(LOG_DIR_FOR_TF_SUMMARY)
+if not os.path.exists(LOG_DIR_FOR_LOGGER):
+    os.makedirs(LOG_DIR_FOR_LOGGER)
+    
+   
+
+## CONFUGRE LOGGING
+logger=logging.getLogger('usc')
+logger.setLevel(logging.INFO)
+#logger.setLevel(logging.DEBUG)
+# create file handler and set level to debug
+loggingFileHandler = logging.FileHandler(LOG_DIR_FOR_LOGGER+'/usc-'+str(datetime.datetime.fromtimestamp(time.time()).strftime('%Y.%m.%d_%H.%M.%S'))+'.log')
+loggingFileHandler.setLevel(logging.DEBUG)
+# create console handler and set level to debug
+loggingConsoleHandler = logging.StreamHandler()
+loggingConsoleHandler.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+loggingFileHandler.setFormatter(formatter)
+loggingConsoleHandler.setFormatter(formatter)
+logger.addHandler(loggingFileHandler)
+logger.addHandler(loggingConsoleHandler)
+#logger.debug('debug message')
+#logger.info('info message')
+#logger.warn('warn message')
+#logger.error('error message')
+#logger.critical('critical message')
+
+##
+## CONFIGURE TF.SUMMARY
+##
+## ONE VARIABLE , TWO WRITERS TO OBTAIN TWO GRPAHS ON THE SAME IMAGE
+trainingAccuracyWriter = tf.summary.FileWriter(LOG_DIR_FOR_TF_SUMMARY+"/trainingAccuracyWriter")
+testAccuracyWriter =tf.summary.FileWriter(LOG_DIR_FOR_TF_SUMMARY+"/testAccuracyWriter")
+tf_summary_accuracy_log_var = tf.Variable(0.0)
+tf.summary.scalar("Accuracy (Test/Train)", tf_summary_accuracy_log_var)
+tfSummaryAccuracyMergedWriter = tf.summary.merge_all()
+
+trainingTimeWriter = tf.summary.FileWriter(LOG_DIR_FOR_TF_SUMMARY+"/trainingTimeWriter")
+testTimeWriter =tf.summary.FileWriter(LOG_DIR_FOR_TF_SUMMARY+"/testTimeWriter")
+tf_summary_time_log_var = tf.Variable(0.0)
+tf.summary.scalar("Time (Test/Train)", tf_summary_time_log_var)
+tfSummaryTimeMergedWriter = tf.summary.merge_all()
+
+
+
+##
+## DATA CONSTANTS
+##
+# 1 RECORD is 4 seconds = 4 x sampling rate double values = 4 x 22050 = 88200 = (2^3) x ( 3^2) x (5^2) x (7^2)
+SOUND_RECORD_SAMPLING_RATE=22050
+DURATION=4
+TRACK_LENGTH=DURATION*SOUND_RECORD_SAMPLING_RATE
+# 10 types of sounds exist (car horn, ambulence, street music, children playing ...)
+NUMBER_OF_CLASSES=10
+
+MAX_NUMBER_OF_SYNTHETIC_FREQUENCY_PER_SAMPLE=4
+MIN_HEARING_FREQUENCY=20
+#MAX_HEARING_FREQUENCY=20000
+MAX_HEARING_FREQUENCY=10000 #after 10000 to 20000 it is hardly heard.
+###        Humans can hear 20Hz to 20 000Hz
+###        Human  voice frq : 100 to 10000 Hz
+###        Human  talk voice frq : 100 to 8000 Hz
+
+
+
+
+##
+## GENERAL CONSTANTS
+##
+OUTPUT_SIZE=NUMBER_OF_CLASSES
+INPUT_SIZE=TRACK_LENGTH
+
+
+
+##
+## FULLY CONNECTED LAYER PARAMETERS
+##
+DROP_OUT=0.5
+KEEP_PROB=DROP_OUT
+
+FOURIER_CNN_KERNEL_COUNTS       = np.array([ 32,32,64])
+FOURIER_CNN_KERNEL_X_SIZES      = np.array([  1, 1, 1])
+FOURIER_CNN_KERNEL_Y_SIZES      = np.array([ 64,32,16])
+FOURIER_CNN_STRIDE_X_SIZES      = np.array([  1, 1, 1])
+FOURIER_CNN_STRIDE_Y_SIZES      = np.array([  1, 1, 1])
+FOURIER_CNN_POOL_X_SIZES        = np.array([  1, 1, 1])
+FOURIER_CNN_POOL_Y_SIZES        = np.array([ 16, 8, 4])
+
+FULLY_CONNECTED_LAYERS=[512,512]
+METRIC_FULLY_CONNECTED_LAYERS=[1024] # DICRIMINATOR
+
+
+##
+## CNN PARAMETERS
+##
+## AUDIO DATA IS ONE DIMENSIONAL  ( that is why *x* is 1)
+#CNN_KERNEL_COUNTS       = np.array([128,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32])
+#CNN_KERNEL_X_SIZES      = np.array([ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
+#CNN_KERNEL_Y_SIZES      = np.array([ 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3])
+#CNN_STRIDE_X_SIZES      = np.array([ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
+#CNN_STRIDE_Y_SIZES      = np.array([ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
+#CNN_POOL_X_SIZES        = np.array([ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
+#CNN_POOL_Y_SIZES        = np.array([ 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
+
+CNN_KERNEL_COUNTS       = np.array([ 16,16,16,16,16,16,16])
+CNN_KERNEL_X_SIZES      = np.array([  1, 1, 1, 1, 1, 1, 1])
+CNN_KERNEL_Y_SIZES      = np.array([  4, 4, 4, 4, 4, 4, 4])
+CNN_STRIDE_X_SIZES      = np.array([  1, 1, 1, 1, 1, 1, 1])
+CNN_STRIDE_Y_SIZES      = np.array([  1, 1, 1, 1, 1, 1, 1])
+CNN_POOL_X_SIZES        = np.array([  1, 1, 1, 1, 1, 1, 1])
+CNN_POOL_Y_SIZES        = np.array([  2, 2, 2, 2, 2, 1, 1])
+
+METRIC_CNN_KERNEL_COUNTS       = np.array([ 16,16,16])
+METRIC_CNN_KERNEL_X_SIZES      = np.array([  1, 1, 1])
+METRIC_CNN_KERNEL_Y_SIZES      = np.array([  4, 4, 4])
+METRIC_CNN_STRIDE_X_SIZES      = np.array([  1, 1, 1])
+METRIC_CNN_STRIDE_Y_SIZES      = np.array([  1, 1, 1])
+METRIC_CNN_POOL_X_SIZES        = np.array([  1, 1, 1])
+METRIC_CNN_POOL_Y_SIZES        = np.array([  1, 1, 1])
+
+CUT_INTO_PARTS_NUMBER=80
+
+##
+## TRAINING PARAMETERS
+##
+#LEARNING_RATE = 0.00001
+#LEARNING_RATE = 0.000001
+LEARNING_RATE = 0.00005
+LEARNING_RATE_BETA1 = 0.9
+LEARNING_RATE_BETA2 = 0.999
+
+LOSS_WEIGHT_1=9/20
+LOSS_WEIGHT_2=9/20
+LOSS_WEIGHT_3=2/20
+
+
+TRAINING_ITERATIONS=9999
+MINI_BATCH_SIZE=10
+
+##
+## GLOBAL VARIABLES
+##
+EPSILON = 1e-4
+MAX_VALUE_FOR_NORMALIZATION=0
+MIN_VALUE_FOR_NORMALIZATION=0
+fold_data_dictionary=dict()
+config = tf.ConfigProto()
+config.gpu_options.allow_growth=True
+#sess = tf.InteractiveSession(config=config)
+
+LAST_AUGMENTATION_CHOICE=0
+
---
+++ src/4.0.1.16-SplitInput-FourierCNN-CNN7-FC2-CNNMetric/main.py
@@ -0,0 +1,184 @@
+#!/usr/bin/env python3
+from header import *
+from data import *
+from NeuralNetworkModel import *
+
+def main(_):
+  global fold_data_dictionary,MAX_VALUE_FOR_NORMALIZATION,MIN_VALUE_FOR_NORMALIZATION
+  # if not already done  : Download from internet , convert to  csv files.   
+  prepareData()
+  # load all data into the memory
+  MAX_VALUE_FOR_NORMALIZATION,MIN_VALUE_FOR_NORMALIZATION=load_all_np_data_back_to_memory(fold_data_dictionary)
+  # normalize all the data
+#  logger.info("##############################################################")
+#  logger.info("MIN_VALUE_FOR_NORMALIZATION : "+str(MIN_VALUE_FOR_NORMALIZATION))
+#  logger.info("MAX_VALUE_FOR_NORMALIZATION : "+str(MAX_VALUE_FOR_NORMALIZATION))
+#  logger.info("fold_data_dictionary['fold1'][0]: "+str(fold_data_dictionary['fold1'][0]))
+#  logger.info("fold_data_dictionary['fold2'][0]: "+str(fold_data_dictionary['fold2'][0]))
+#  logger.info("fold_data_dictionary['fold3'][0]: "+str(fold_data_dictionary['fold3'][0]))
+#  logger.info("fold_data_dictionary['fold4'][0]: "+str(fold_data_dictionary['fold4'][0]))
+#  logger.info("fold_data_dictionary['fold5'][0]: "+str(fold_data_dictionary['fold5'][0]))
+#  logger.info("fold_data_dictionary['fold6'][0]: "+str(fold_data_dictionary['fold6'][0]))
+#  logger.info("fold_data_dictionary['fold7'][0]: "+str(fold_data_dictionary['fold7'][0]))
+#  logger.info("fold_data_dictionary['fold8'][0]: "+str(fold_data_dictionary['fold8'][0]))
+#  logger.info("fold_data_dictionary['fold9'][0]: "+str(fold_data_dictionary['fold9'][0]))
+#  logger.info("fold_data_dictionary['fold10'][0]: "+str(fold_data_dictionary['fold10'][0]))
+#  logger.info("##############################################################")
+  normalize_all_data(fold_data_dictionary,MAX_VALUE_FOR_NORMALIZATION,MIN_VALUE_FOR_NORMALIZATION)
+  
+#  logger.info("##############################################################")
+#  logger.info("MIN_VALUE_FOR_NORMALIZATION : "+str(MIN_VALUE_FOR_NORMALIZATION))
+#  logger.info("MAX_VALUE_FOR_NORMALIZATION : "+str(MAX_VALUE_FOR_NORMALIZATION))
+#  logger.info("fold_data_dictionary['fold1'][0]: "+str(fold_data_dictionary['fold1'][0]))
+#  logger.info("fold_data_dictionary['fold2'][0]: "+str(fold_data_dictionary['fold2'][0]))
+#  logger.info("fold_data_dictionary['fold3'][0]: "+str(fold_data_dictionary['fold3'][0]))
+#  logger.info("fold_data_dictionary['fold4'][0]: "+str(fold_data_dictionary['fold4'][0]))
+#  logger.info("fold_data_dictionary['fold5'][0]: "+str(fold_data_dictionary['fold5'][0]))
+#  logger.info("fold_data_dictionary['fold6'][0]: "+str(fold_data_dictionary['fold6'][0]))
+#  logger.info("fold_data_dictionary['fold7'][0]: "+str(fold_data_dictionary['fold7'][0]))
+#  logger.info("fold_data_dictionary['fold8'][0]: "+str(fold_data_dictionary['fold8'][0]))
+#  logger.info("fold_data_dictionary['fold9'][0]: "+str(fold_data_dictionary['fold9'][0]))
+#  logger.info("fold_data_dictionary['fold10'][0]: "+str(fold_data_dictionary['fold10'][0]))
+#  logger.info("##############################################################")
+  
+
+
+  with tf.Session() as session:
+    
+   neuralNetworkModel=NeuralNetworkModel(session,logger)
+   saver = tf.train.Saver()
+
+
+   ##
+   ## INITIALIZE SESSION
+   ##
+   checkpoint= tf.train.get_checkpoint_state(os.path.dirname(SAVE_DIR+'/usc_model'))
+   if checkpoint and checkpoint.model_checkpoint_path:
+    saver.restore(session,checkpoint.model_checkpoint_path)
+   else : 
+    session.run(tf.global_variables_initializer())
+   ##
+   ##
+    
+       
+   for trainingIterationNo in range(TRAINING_ITERATIONS):
+        
+    logger.info("##############################################################")
+    logger.info("Training Iteration : "+str(trainingIterationNo))
+        
+    prepareDataTimes=[ ]
+    trainingTimes=[ ]
+    trainingAccuracies_1=[ ]
+    trainingAccuracies_2=[ ]
+    trainingAccuracies_metric=[ ]
+    testTimes=[ ]
+    testAccuracies_1=[ ]
+    testAccuracies_2=[ ]
+    testAccuracies_metric=[ ]
+    tainingLosses_metric=[]
+    for fold in np.random.permutation(FOLD_DIRS):
+       if fold == "fold10":
+         logger.info(" Starting Fold Testing : "+fold)
+       else :
+         logger.info(" Starting Fold Training : "+fold)
+       current_fold_data=get_fold_data(fold)
+       for current_batch_counter in range(int(current_fold_data.shape[0]/MINI_BATCH_SIZE)) :
+         #if current_batch_counter % 10 == 0 :
+         #  logger.info(" Batch Counter : "+str(current_batch_counter))
+         if (current_batch_counter+1)*MINI_BATCH_SIZE <= current_fold_data.shape[0] :
+           batch_data=current_fold_data[current_batch_counter*MINI_BATCH_SIZE:(current_batch_counter+1)*MINI_BATCH_SIZE,:]
+         else:
+           batch_data=current_fold_data[current_batch_counter*MINI_BATCH_SIZE:,:]
+         if fold == "fold10":
+             ## FOLD10 is reserved for testing
+              testTime,testAccuracy_1,testAccuracy_2,testAccuracy_metric=neuralNetworkModel.test(batch_data)
+              testTimes.append(testTime)
+              testAccuracies_1.append(testAccuracy_1)
+              testAccuracies_2.append(testAccuracy_2)
+              testAccuracies_metric.append(testAccuracy_metric)
+         else:
+              batch_data_1=batch_data
+              #batch_data_2=np.random.permutation(batch_data)
+              batch_data_2=np.copy(batch_data)
+              for i in range(int(batch_data_2.shape[0]/2)):
+                 batch_data_2[i]=batch_data_2[i+1]
+              trainingTime,trainingAccuracy_1,trainingAccuracy_2,trainingAccuracy_metric,loss_adverserial,prepareDataTime=neuralNetworkModel.train(batch_data_1,batch_data_2)
+              trainingTimes.append(trainingTime)
+              trainingAccuracies_1.append(trainingAccuracy_1)
+              trainingAccuracies_2.append(trainingAccuracy_2)
+              trainingAccuracies_metric.append(trainingAccuracy_metric)
+              tainingLosses_metric.append(loss_adverserial)
+              
+              prepareDataTimes.append(prepareDataTime)
+
+
+
+    ## LOGGING            
+    logger.info("Prepare Data Time : "+str(np.sum(prepareDataTimes)))
+    logger.info("Training Time : "+str(np.sum(trainingTimes)))
+    logger.info("Mean Training Accuracy_1 : "+str(np.mean(trainingAccuracies_1)))
+    logger.info("Max Training Accuracy_1 : "+str(np.max(trainingAccuracies_1)))
+    logger.info("Min Training Accuracy_1 : "+str(np.min(trainingAccuracies_1)))
+    logger.info("Mean Training Accuracy_2 : "+str(np.mean(trainingAccuracies_2)))
+    logger.info("Max Training Accuracy_2 : "+str(np.max(trainingAccuracies_2)))
+    logger.info("Min Training Accuracy_2 : "+str(np.min(trainingAccuracies_2)))
+    logger.info("Mean Training Accuracy_metric : "+str(np.mean(trainingAccuracies_metric)))
+    logger.info("Max Training Accuracy_metric : "+str(np.max(trainingAccuracies_metric)))
+    logger.info("Min Training Accuracy_metric : "+str(np.min(trainingAccuracies_metric)))
+    logger.info("Mean Training Loss Metric : "+str(np.mean(tainingLosses_metric)))
+    logger.info("Max Training  Loss Metric : "+str(np.max(tainingLosses_metric)))
+    logger.info("Min Training  Loss Metric : "+str(np.min(tainingLosses_metric)))
+    logger.info("Test Time : "+str(np.sum(testTimes)))
+    if len(testAccuracies_1) > 0 :
+      logger.info("Mean Test Accuracy_1 : "+str(np.mean(testAccuracies_1)))
+      logger.info("Max Test Accuracy_1 : "+str(np.max(testAccuracies_1)))
+      logger.info("Min Test Accuracy_1 : "+str(np.min(testAccuracies_1)))
+      logger.info("Mean Test Accuracy_2 : "+str(np.mean(testAccuracies_2)))
+      logger.info("Max Test Accuracy_2 : "+str(np.max(testAccuracies_2)))
+      logger.info("Min Test Accuracy_2 : "+str(np.min(testAccuracies_2)))
+      logger.info("Mean Test Accuracy_metric : "+str(np.mean(testAccuracies_metric)))
+      logger.info("Max Test Accuracy_metric : "+str(np.max(testAccuracies_metric)))
+      logger.info("Min Test Accuracy_metric : "+str(np.min(testAccuracies_metric)))
+    
+    ## GRAPH (FOR LOGGING)
+    tariningAcuracySummary = session.run(tfSummaryAccuracyMergedWriter, {tf_summary_accuracy_log_var: np.mean(trainingAccuracies_1)})
+    trainingAccuracyWriter.add_summary(tariningAcuracySummary, trainingIterationNo)
+    trainingAccuracyWriter.flush()
+
+    tariningAcuracySummary = session.run(tfSummaryAccuracyMergedWriter, {tf_summary_accuracy_log_var: np.mean(trainingAccuracies_2)})
+    trainingAccuracyWriter.add_summary(tariningAcuracySummary, trainingIterationNo)
+    trainingAccuracyWriter.flush()
+
+    testAcuracySummary = session.run(tfSummaryAccuracyMergedWriter, {tf_summary_accuracy_log_var:np.mean(testAccuracies_1)})
+    testAccuracyWriter.add_summary(testAcuracySummary, trainingIterationNo)
+    testAccuracyWriter.flush()
+
+    testAcuracySummary = session.run(tfSummaryAccuracyMergedWriter, {tf_summary_accuracy_log_var:np.mean(testAccuracies_2)})
+    testAccuracyWriter.add_summary(testAcuracySummary, trainingIterationNo)
+    testAccuracyWriter.flush()
+
+    tariningTimeSummary = session.run(tfSummaryTimeMergedWriter, {tf_summary_time_log_var: np.sum(trainingTimes)})
+    trainingTimeWriter.add_summary(tariningTimeSummary, trainingIterationNo)
+    trainingTimeWriter.flush()
+
+    testTimeSummary = session.run(tfSummaryTimeMergedWriter, {tf_summary_time_log_var:np.mean(testTimes)})
+    testTimeWriter.add_summary(testTimeSummary, trainingIterationNo)
+    testTimeWriter.flush()
+
+    if trainingIterationNo>0 and trainingIterationNo%10 == 0 :
+      saver.save(session, SAVE_DIR+'/usc_model',global_step=trainingIterationNo)
+
+
+if __name__ == '__main__':
+ parser = argparse.ArgumentParser()
+ parser.add_argument('--data_dir', type=str,
+                     default='/tmp/tensorflow/mnist/input_data',
+                     help='Directory for storing input data')
+ FLAGS, unparsed = parser.parse_known_args()
+ tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
+
+
+
+
+
+  
