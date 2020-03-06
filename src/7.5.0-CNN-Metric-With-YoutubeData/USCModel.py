b4ab1fff7c1f5c91005a406c088c2db431ea1a0e
mehmetpekmezci <mpekmezci@havelsan.com.tr>
2020-03-07 00:11:12 +0300

    MP

---
+++ src/7.5.0-CNN-Metric-With-YoutubeData/USCModel.py
@@ -0,0 +1,136 @@
+#!/usr/bin/env python3
+from USCHeader import *
+from USCLogger import *
+from USCData import *
+
+##
+## NueralNetworkModel will be as :
+## CNN LAYERS + LSTM LAYERS + FULLY CONNECTED LAYER + SOFTMAX
+##
+class USCModel :
+ def __init__(self, session, uscLogger,uscData): 
+   self.session               = session
+   self.uscLogger             = uscLogger
+   self.uscData               = uscData
+   ## self.uscData.time_slice_length = 440
+
+   script_dir=os.path.dirname(os.path.realpath(__file__))
+   script_name=os.path.basename(script_dir)
+   self.model_save_dir=script_dir+"/../../save/"+script_name
+   self.model_save_file="model.h5"
+
+   ## so we will have nearly 400 time steps in 4 secs record. (88200) (with %50 overlapping)
+   self.lstm_time_steps       = self.uscData.number_of_time_slices
+   self.training_iterations   = 10000
+   self.buildModel()
+
+   self.load_weights()
+   self.trainCount=0
+
+   self.model.summary()
+
+
+
+ #def cutIntoLSTMSTepSizes(self,inputList):
+ #  listOfList=[]
+ #  for c in range(int(self.number_of_time_slices/self.number_of_lstm_time_steps)) :
+ #     chunk=inputList[c:c*self.number_of_lstm_time_steps]
+ #     listOfList.append(chunk)
+ #  return listOfList
+
+ def load_weights(self):
+     if os.path.exists(self.model_save_dir+"/"+self.model_save_file):
+         self.model.load_weights(self.model_save_dir+"/"+self.model_save_file)
+
+ def save_weights(self):
+     if not os.path.exists(self.model_save_dir):
+         os.makedirs(self.model_save_dir)
+     self.model.save_weights(self.model_save_dir+"/"+self.model_save_file)
+
+
+ def prepareData(self,data,augment):
+  x_data=data[:,:4*self.uscData.sound_record_sampling_rate]
+  if augment==True :
+    x_data=self.uscData.augment_random(x_data)
+  x_data=self.uscData.normalize(x_data)
+  y_data=data[:,4*self.uscData.sound_record_sampling_rate]
+  y_data_one_hot_encoded=self.uscData.one_hot_encode_array(y_data)
+  return encodedValue,y_data_one_hot_encoded
+
+
+ def train(self,data):
+  augment=True
+  prepareDataTimeStart = int(round(time.time())) 
+  x_data,y_data=self.prepareData(data,augment)
+  prepareDataTimeStop = int(round(time.time())) 
+  prepareDataTime=prepareDataTimeStop-prepareDataTimeStart
+  trainingTimeStart = int(round(time.time())) 
+
+  self.model.fit(x_data, y_data, epochs = 1, batch_size = self.uscData.mini_batch_size,verbose=0)
+  trainingTimeStop = int(round(time.time())) 
+  trainingTime=trainingTimeStop-trainingTimeStart
+  evaluation = self.model.evaluate(x_data, y_data, batch_size = self.uscData.mini_batch_size,verbose=0)
+  trainingAccuracy = evaluation[1]
+  #print(self.model.metrics_names) 
+  #print(evaluation) 
+  self.trainCount+=1
+  if self.trainCount % 100 == 0 :
+     self.save_weights()
+
+  return trainingTime,trainingAccuracy,prepareDataTime
+     
+ def test(self,data):
+  testTimeStart = int(round(time.time())) 
+  augment=False
+  x_data,y_data=self.prepareData(data,augment) 
+  evaluation = self.model.evaluate(x_data, y_data,batch_size = self.uscData.mini_batch_size,verbose=0)
+  testAccuracy = evaluation[1]
+  testTimeStop = int(round(time.time())) 
+  testTime=testTimeStop-testTimeStart
+  return testTime,testAccuracy
+  
+# def buildModel(self):
+#   layer_input = keras.layers.Input(batch_shape=(self.uscData.mini_batch_size,self.uscData.number_of_time_slices,self.uscData.latent_space_presentation_data_length))
+#   out=keras.layers.LSTM(self.lstm_size,dropout=0.2,recurrent_dropout=0.2)(layer_input)
+#   out=keras.layers.BatchNormalization()(out)
+#   out=keras.layers.Dense(units = 2048,activation='relu')(out)
+#   out=keras.layers.BatchNormalization()(out)
+#   out=keras.layers.Dropout(0.2)(out)
+#   out=keras.layers.Dense(units = 2048,activation='relu')(out)
+#   out=keras.layers.BatchNormalization()(out)
+#   out=keras.layers.Dropout(0.2)(out)
+#   out=keras.layers.Dense(units = self.uscData.number_of_classes,activation='softmax')(out)
+#   self.model = keras.models.Model(inputs=[layer_input], outputs=[out])
+#   selectedOptimizer=keras.optimizers.Adam(lr=0.0001)
+#   self.model.compile(optimizer=selectedOptimizer, loss='categorical_crossentropy',metrics=['accuracy'])
+
+
+
+ def buildModel(self):
+   #layer_input = keras.layers.Input(batch_shape=(self.uscData.mini_batch_size,self.uscData.track_length,1))
+   layer_input = keras.layers.Input(batch_shape=(self.uscData.mini_batch_size,self.uscData.latent_space_presentation_data_length,1))
+   # Convolution1D(filters, kernel_size,...)
+   out=keras.layers.Convolution1D(16, 4,strides=2,activation='relu', border_mode='same')(layer_input)
+   out=keras.layers.BatchNormalization()(out)
+   out=keras.layers.Convolution1D(16, 4,strides=2,activation='relu', border_mode='same')(out)
+   out=keras.layers.BatchNormalization()(out)
+   out=keras.layers.Convolution1D(16, 4,strides=2,activation='relu', border_mode='same')(out)
+   out=keras.layers.BatchNormalization()(out)
+   out=keras.layers.Convolution1D(16, 4,activation='relu', border_mode='same')(out)
+   out=keras.layers.BatchNormalization()(out)
+   out=keras.layers.Convolution1D(16, 4,activation='relu', border_mode='same')(out)
+   out=keras.layers.BatchNormalization()(out)
+   out=keras.layers.Convolution1D(16, 4,activation='relu', border_mode='same')(out)
+   out=keras.layers.BatchNormalization()(out)
+   out=keras.layers.Flatten()(out)
+   out=keras.layers.BatchNormalization()(out)
+   out=keras.layers.Dense(units = 32,activation='sigmoid')(out)
+   out=keras.layers.BatchNormalization()(out)
+   out=keras.layers.Dense(units = 128,activation='sigmoid')(out)
+   out=keras.layers.BatchNormalization()(out)
+   out=keras.layers.Dense(units = self.uscData.number_of_classes,activation='softmax')(out)
+   self.model = keras.models.Model(inputs=[layer_input], outputs=[out])
+   selectedOptimizer=keras.optimizers.Adam(lr=0.0001)
+   self.model.compile(optimizer=selectedOptimizer, loss='categorical_crossentropy',metrics=['accuracy'])
+
+
---
+++ src/7.5.0-CNN-Metric-With-YoutubeData/main.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+from USCHeader import *
+from USCLogger import *
+from USCData import *
+from USCModel import *
+
+
+def main(_):
+ uscLogger=USCLogger()
+ uscData=USCData(uscLogger.logger)
+ uscData.prepareData()
+ uscData.findListOfYoutubeDataFiles()
+  
+ config = tf.ConfigProto()
+ config.gpu_options.allow_growth = True
+
+ with tf.Session(config=config) as session:
+   session.run(tf.global_variables_initializer())
+
+   uscModel=USCModel(session,uscLogger,uscData)
+   for trainingIterationNo in range(uscModel.training_iterations):
+    uscLogger.logStepStart(session,trainingIterationNo)
+    prepareDataTimes=[ ]
+    trainingTimes=[ ]
+    trainingAccuracies=[ ]
+    testTimes=[ ]
+    testAccuracies=[ ]
+    ## youtube dta also contain fold1-9 of urban sound data 
+    current_youtube_data_as_list=np.random.permutation(uscData.loadNextYoutubeData())
+    for current_batch_counter in range(math.floor(len(current_youtube_data_as_list)/uscData.mini_batch_size)) :
+         if current_batch_counter % 100 == 0:
+           uscLogger.logger.info("Training Batch No : "+str(current_batch_counter))
+         batch_data=current_youtube_data_as_list[current_batch_counter*uscData.mini_batch_size:(current_batch_counter+1)*uscData.mini_batch_size]
+         #uscLogger.logger.info("batch_data.shape: "+str(batch_data.shape))
+         trainingTime,trainingAccuracy,prepareDataTime=uscModel.train(batch_data)
+         trainingTimes.append(trainingTime)
+         trainingAccuracies.append(trainingAccuracy)
+         prepareDataTimes.append(prepareDataTime)
+    uscLogger.logger.info("Testing with fol10 of urban sound data... ")
+    ## FOLD10 is reserved for testing
+    current_fold_data==np.random.permutation(uscData.get_fold_data("fold10"))
+    for current_batch_counter in range(int(current_fold_data.shape[0]/uscData.mini_batch_size)) :
+         if (current_batch_counter+1)*uscData.mini_batch_size <= current_fold_data.shape[0] :
+           batch_data=current_fold_data[current_batch_counter*uscData.mini_batch_size:(current_batch_counter+1)*uscData.mini_batch_size,:]
+         else:
+           batch_data=current_fold_data[current_batch_counter*uscData.mini_batch_size:,:]
+         #uscLogger.logger.info("  Testing Batch Counter : "+str(current_batch_counter))
+         testTime,testAccuracy=uscModel.test(batch_data)
+         testTimes.append(testTime)
+         testAccuracies.append(testAccuracy)
+    uscLogger.logStepEnd(session,prepareDataTimes,trainingTimes,trainingAccuracies,testTimes,testAccuracies,trainingIterationNo)
+
+if __name__ == '__main__':
+ tf.app.run(main=main)
+
