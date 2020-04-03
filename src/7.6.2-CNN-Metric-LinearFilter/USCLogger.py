dd6cdbb7bba07093af598039d097eb82ef2af28b
Mehmet Pekmezci <mpekmezci@havelsan.com.tr>
2020-04-04 00:06:20 +0300

    MP

--- src/7.6.2-CNN-Metric-LinearFilter/USCLogger.py
+++ src/7.6.2-CNN-Metric-LinearFilter/USCLogger.py
@@ -72,6 +72,8 @@ class USCLogger :
 
  def printConfusionMatrix(self):
     self.logger.info("-------------------------------------")
+    self.logger.info("Confusion Matrix for the first Classifier : ")
+    self.logger.info("-------------------------------------")
     #self.logger.info("0 = air_conditioner, 1 = car_horn , 2 = children_playing , 3 = dog_bark , 4 = drilling , 5 = engine_idling , 6 = gun_shot , 7 = jackhammer , 8 = siren , 9 = street_music")
     self.logger.info("%-15.15s %-15.15s %-15.15s %-15.15s %-15.15s %-15.15s %-15.15s %-15.15s %-15.15s %-15.15s  " % 
                        ('air_conditioner',
