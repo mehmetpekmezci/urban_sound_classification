a4690bd12b8d1252103d65ad5b8d6bb825bc9b6b
Mehmet Pekmezci <mpekmezci@havelsan.com.tr>
2020-05-02 23:53:24 +0300

    MP

--- youtube/convertAll.sh
+++ youtube/convertAll.sh
@@ -1,7 +1,7 @@
 cd downloads
-for i in *.m4a
+for i in *.m4a *.mp3
 do
-	f=$(echo $i | sed -e 's/.m4a//')
+	f=$(echo $i | sed -e 's/.m4a//'| sed -e 's/.mp3//')
         ffmpeg -i $i -acodec pcm_s16le -ar 44100 $f.wav 
 done
 
